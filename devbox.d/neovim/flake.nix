fb3866f219d88a2a36fe04fd7d9cdd515b99fe99
Rodrigo Belem <rodrigo.belem@gmail.com>
2024-01-31 20:38:06 -0400

    Switch to neovim nightly build to use the latest regex features

--- devbox.d/neovim/flake.nix
+++ devbox.d/neovim/flake.nix
@@ -3,26 +3,37 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, neovim-nightly-overlay, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        neovim-full = pkgs.neovim.override {
-          extraLuaPackages = ps: [ ps.lpeg ];
-          viAlias = true;
-          vimAlias = true;
-          withNodeJs = true;
-          withPython3 = true;
-          withRuby = true;
+        overlay = final: prev: {
+          neovim-full = prev.neovim.override {
+            extraLuaPackages = ps: [ ps.lpeg ];
+            viAlias = true;
+            vimAlias = true;
+            withNodeJs = true;
+            withPython3 = true;
+            withRuby = true;
+          };
         };
-      in
-      {
+
+        pkgs =
+          import nixpkgs {
+            inherit system;
+            overlays = [
+              neovim-nightly-overlay.overlay
+              overlay
+            ];
+          };
+
+      in rec {
         packages = {
-          inherit neovim-full;
-          default = neovim-full;
+          inherit pkgs;
+          default = pkgs.neovim-full;
         };
       }
     );
