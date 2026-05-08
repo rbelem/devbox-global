8ace5b4fff49c54b1676c067e7d6fbeab2c26a92
Rodrigo Belem <rodrigo.belem@gmail.com>
2026-05-08 18:52:33 -0400

    build(agent-browser): update to 0.27.0 and restrict platforms
    Updates the agent-browser package to version 0.27.0 and limits
    supported systems to x86_64-linux with the new release hash.

--- devbox.d/agent-browser/flake.nix
+++ devbox.d/agent-browser/flake.nix
@@ -4,18 +4,15 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }: let
-    supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    supportedSystems = [ "x86_64-linux" ];
     forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f nixpkgs.legacyPackages.${system});
 
     # Map nix system to agent-browser release asset name
     systemToTarget = {
       "x86_64-linux"   = "agent-browser-linux-x64";
-      "aarch64-linux"  = "agent-browser-linux-arm64";
-      "x86_64-darwin"  = "agent-browser-darwin-x64";
-      "aarch64-darwin" = "agent-browser-darwin-arm64";
     };
 
-    version = "0.26.0";
+    version = "0.27.0";
 
   in {
     packages = forAllSystems (pkgs: rec {
@@ -26,10 +23,8 @@
         src = pkgs.fetchurl {
           url = "https://github.com/vercel-labs/agent-browser/releases/download/v${version}/${systemToTarget.${pkgs.stdenv.hostPlatform.system}}";
           sha256 = {
-            "x86_64-linux"   = "1hz3k329n6m4szfcbblr1ksplf2nv5vmdd2iww2fwwmzk8jxr147";
-            "aarch64-linux"  = "0is4vf19nsw7rn65qdnb1r8qlgla0rvcbrfa3x8ycv4g54bip45k";
-            "x86_64-darwin"  = "1nw38bfp95a0dm6x00fmhanq0sx6xh2v8bb8ywjz83s3hm28y6gm";
-            "aarch64-darwin" = "0l16mslkldln7ahln1lqinln6gplgy6jn4b41zc2nlmbaxyazxqq";
+            #"x86_64-linux"   = pkgs.lib.fakeHash;
+            "x86_64-linux"   = "sha256-wkmpSMtqYN+UMg4xnDcqXmeUZsqOosxOabrE/MfCcPM=";
           }.${pkgs.stdenv.hostPlatform.system};
         };
 
