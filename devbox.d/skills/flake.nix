e58bdacc7399c615e38cdced493877e241d99c90
Rodrigo Belem <rodrigo.belem@gmail.com>
2026-05-16 10:38:53 -0400

    chore(skills): update package to v1.5.7

--- devbox.d/skills/flake.nix
+++ devbox.d/skills/flake.nix
@@ -12,12 +12,12 @@
       packages = forAllSystems (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          version = "1.5.1";
+          version = "1.5.7";
           src = pkgs.fetchFromGitHub {
             owner = "vercel-labs";
             repo = "skills";
             rev = "v${version}";
-            hash = "sha256-JVJeottMyjxdiGPS7O4QsshKdbwbYcKMvwe/PB7I/Zw=";
+            hash = "sha256-Dzp0Gx+EcO7daxLTZ0QpMu4EEYdDWWEE8b5RF4Fv9QM=";
           };
         in
         {
@@ -27,7 +27,7 @@
               pname = oldAttrs.pname;
               inherit version src;
               fetcherVersion = 3;
-              hash = "sha256-0CS6BTjTj/TAnMNahTk4Vt/0/2eMxmCGUV9PwI8l4Ao=";
+              hash = "sha256-3GSa4ze859dRA4Yrxw8r3rwZKn7FMSjBMvpz1HTDobU=";
             };
           });
         }
