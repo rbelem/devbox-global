3efbb93fb392d3be9dca9da6fa239485d19ed5cd
Rodrigo Belem <rodrigo.belem@gmail.com>
2026-05-24 18:31:00 -0400

    build(whichllm): bump from 0.5.3 to 0.5.7

--- devbox.d/whichllm/flake.nix
+++ devbox.d/whichllm/flake.nix
@@ -7,7 +7,7 @@
     supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
     forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
-    version = "0.5.3";
+    version = "0.5.7";
 
     # Get the real hash:
     #   cd ~/.local/share/devbox/global/current/devbox.d/whichllm
@@ -15,8 +15,8 @@
     #   run the command "devbox global update"
     # Then paste the sha256-... value below
     #
-    # srcHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-    srcHash = "sha256-4uHcFUnqhsBS931OVn8MRHBrrAZhOzVTtkvhPgtIN8U=";
+    # srcHash = nixpkgs.lib.fakeHash;
+    srcHash = "sha256-UvhCSC9tKpdgXFCMGn0HWM0kuHhXSauEr/ys/9PUIRs=";
   in {
     packages = forAllSystems (system:
       let
