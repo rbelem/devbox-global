45ff80817969eed47b8ec0f6227deb441341b645
Rodrigo Belem <rodrigo.belem@gmail.com>
2026-07-23 09:52:42 -0400

    chore(flakes): bump agent-browser 0.32.3->0.32.4

--- devbox.d/agent-browser/flake.nix
+++ devbox.d/agent-browser/flake.nix
@@ -12,7 +12,7 @@
       "x86_64-linux"   = "agent-browser-linux-x64";
     };
 
-    version = "0.32.3";
+    version = "0.32.4";
 
   in {
     packages = forAllSystems (pkgs: rec {
@@ -23,7 +23,7 @@
         src = pkgs.fetchurl {
           url = "https://github.com/vercel-labs/agent-browser/releases/download/v${version}/${systemToTarget.${pkgs.stdenv.hostPlatform.system}}";
           sha256 = {
-            "x86_64-linux"   = "sha256-JD9uAcS33qU60H2XVN+ZAzxhRYLVxoXFKaHLgcr8OrE=";
+            "x86_64-linux"   = "sha256-YrohiTRV6eZdYE8YO1mjUgJZkAP5erw+B619rRVFhFs=";
           }.${pkgs.stdenv.hostPlatform.system};
         };
 
